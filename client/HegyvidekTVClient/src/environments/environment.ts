2619c8626f6b207be3f15eda9f630c4f48475c8d
Benedek Vass <vabe44@gmail.com>
2018-03-30 16:05:53 +0200

    port change

--- client/HegyvidekTVClient/src/environments/environment.ts
+++ client/HegyvidekTVClient/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'http://localhost:3001/api',
   weatherApiKey: '2d51d2c195bd413f69941502a3e9282a',
   facebook: 'https://www.facebook.com/hegyvidektelevizio',
   youtube: 'https://www.youtube.com/user/hegyvidektv'
