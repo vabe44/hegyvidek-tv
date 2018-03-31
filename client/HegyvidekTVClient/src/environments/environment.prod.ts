e848854a44f4b6ffb401324e5eba0527d6a2bde3
Benedek Vass <vabe44@gmail.com>
2018-03-31 02:55:17 +0200

    changed paths

--- client/HegyvidekTVClient/src/environments/environment.prod.ts
+++ client/HegyvidekTVClient/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: 'http://htv.vabe.design/api',
   weatherApiKey: '2d51d2c195bd413f69941502a3e9282a',
   facebook: 'https://www.facebook.com/hegyvidektelevizio',
   youtube: 'https://www.youtube.com/user/hegyvidektv'
